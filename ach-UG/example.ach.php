a266c984f8e1dc6526da406b1ba7a6344d24f7c3
marianadiediukh <30461094+marianadiediukh@users.noreply.github.com>
2017-11-09 02:07:59 +0200

    New translations example.php (Acholi)

---
+++ ach-UG/example.ach.php
@@ -0,0 +1,10 @@
+﻿<?php
+$en_lang = array (
+"Identifier_1"=>'crwdns124769:0crwdne124769:0',
+"Identifier_2"=>'crwdns124770:0crwdne124770:0',
+"Identifier_3"=>'crwdns124771:0crwdne124771:0',
+"Identifier_4"=>'crwdns124772:0crwdne124772:0',
+"Identifier_5"=>'crwdns124773:0crwdne124773:0',
+);
+extract($en_lang);
+?>

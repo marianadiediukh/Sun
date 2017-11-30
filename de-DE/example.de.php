7e98ba93da002d5cc29e904dc3e710b196eb2484
marianadiediukh <30461094+marianadiediukh@users.noreply.github.com>
2017-11-30 06:50:36 +0200

    New translations example.php (German)

--- de-DE/example.de.php
+++ de-DE/example.de.php
@@ -1,6 +1,6 @@
 ﻿<?php
 $en_lang = array (
-"Identifier_1"=>'String for translation 1',
+"Identifier_1"=>'Zeichenfolge für die Übersetzung #1',
 "Identifier_2"=>'String for translation 2',
 "Identifier_3"=>'String for translation 3',
 "Identifier_4"=>'String for translation 4',

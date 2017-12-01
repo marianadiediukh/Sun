3055f6db390b82883936678c648811dad5383191
marianadiediukh <30461094+marianadiediukh@users.noreply.github.com>
2017-12-02 01:11:20 +0200

    New translations example.php (Spanish)

--- es-ES/example.es.php
+++ es-ES/example.es.php
@@ -1,10 +1,10 @@
 ﻿<?php
 $en_lang = array (
-"Identifier_1"=>'String for translation 1',
-"Identifier_2"=>'String for translation 2',
-"Identifier_3"=>'String for translation 3',
-"Identifier_4"=>'String for translation 4',
-"Identifier_5"=>'String for translation 5',
+"Identifier_1"=>'Ghhghvh',
+"Identifier_2"=>'Cadena de traducción #2',
+"Identifier_3"=>'Cadena de traducción #2',
+"Identifier_4"=>'Cadena de traducción #2',
+"Identifier_5"=>'Cadena de traducción #2',
 );
 extract($en_lang);
 ?>

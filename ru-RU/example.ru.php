bb6eeb78ef92053192a48e7b965dd2f3870d950a
marianadiediukh <30461094+marianadiediukh@users.noreply.github.com>
2018-01-24 09:11:04 +0200

    New translations example.php (Russian)

--- ru-RU/example.ru.php
+++ ru-RU/example.ru.php
@@ -1,8 +1,8 @@
 ﻿<?php
 $en_lang = array (
-"Identifier_1"=>'String for translation 1',
-"Identifier_2"=>'String for translation 2',
-"Identifier_3"=>'String for translation 3',
+"Identifier_1"=>'Строка для перевода 1',
+"Identifier_2"=>'Строка для перевода 2',
+"Identifier_3"=>'Строка для перевода 3',
 "Identifier_4"=>'String for translation 4',
 "Identifier_5"=>'String for translation 5',
 );
